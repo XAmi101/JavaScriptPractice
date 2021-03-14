28ae82ff540a80cb763bda7fc2936c3b98155e9c
Ndawi <famousvacancy@gmail.com>
2021-03-13 23:54:46 -0500

    Update TuckInArray.js

--- TuckInArray.js
+++ TuckInArray.js
@@ -13,5 +13,9 @@ Use the spread syntax to solve this challenge.
 **/
 
 function tuckIn(arr1, arr2) {
-	
+	arr1.splice(2, 0, ...arr2);	
+// 	return 
 }
+
+// tuckIn([1, 10], [2, 3, 4, 5, 6, 7, 8, 9])
+console.log(tuckIn([1, 10], [2, 3, 4, 5, 6, 7, 8, 9])); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
