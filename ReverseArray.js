4719a63d2ea6857996b8f09363eab12794c10051
Ndawi <famousvacancy@gmail.com>
2021-05-01 23:50:34 -0400

    Code challenge

---
+++ ReverseArray.js
@@ -0,0 +1,23 @@
+/**
+
+Create a function that takes an array and return the reverse.
+
+**/
+
+
+function reverseArray(ar)
+{
+    let ar2 = [];
+    
+    for(let i = ar.length - 1; i >= 0; i--)
+    {
+        ar2.push(ar[i]);
+    }
+    
+    return ar2;
+}
+
+
+let ar = [1, 2, 3];
+let ar2 = reverseArray(ar);
+println(ar2);
