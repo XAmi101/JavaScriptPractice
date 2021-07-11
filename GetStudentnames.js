2145a47a1680a3240ccadfa2f8a13bcd9d3a9668
Ndawi <famousvacancy@gmail.com>
2021-07-10 22:41:30 -0400

    Update GetStudentnames.js

--- GetStudentnames.js
+++ GetStudentnames.js
@@ -1,4 +1,7 @@
 //Create a function that takes an array of students and returns an array of student names.
 
-function getStudentNames(students) 
+function getStudentNames(students) {
+   let nameArr = students.map(a => a.name);
+   return nameArr;
+}
   
