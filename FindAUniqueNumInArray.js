32fe01530a1a0b6602a497d1435dc8cb79074870
Ndawi <famousvacancy@gmail.com>
2021-07-13 23:01:13 -0400

    Update FindAUniqueNumInArray.js

--- FindAUniqueNumInArray.js
+++ FindAUniqueNumInArray.js
@@ -9,11 +9,15 @@ findSingleNumber([7, 13, 3, 6, 5, 4, 4, 13, 5, 3, 6, 7, 6, 5, 3, 13, 4, 7, 13, 5
 findSingleNumber([1, 2, 3, 6, 5, 4, 4, 2, 5, 3, 6, 1, 6, 5, 3, 2, 4, 1, 2, 5, 1, 4, 3, 6, 101, 4, 3, 1, 5, 6, 2]) ➞ 101
 
 Notes
-Don't forget to return the result.
-Be aware this challenge includes two validations:
-Empty input should return null (example #3).
-Single item arrays should return that item (example #2).
-There are always 1 or 0 unique numbers in the input. No two or three+ uniques.
-If you're stuck or your solution is over complicated check the Resources tab.
+-Don't forget to return the result.
+-Be aware this challenge includes two validations:
+  1. Empty input should return null (example #3).
+  2. Single item arrays should return that item (example #2).
+-There are always 1 or 0 unique numbers in the input. No two or three+ uniques.
+-If you're stuck or your solution is over complicated check the Resources tab.
 
 **/
+
+function findSingleNumber(numbers) {
+	
+}
