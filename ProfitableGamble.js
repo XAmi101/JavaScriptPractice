c715afe82e311d764940612d77cd98f4549c8290
Ndawi <famousvacancy@gmail.com>
2021-09-08 23:43:17 -0400

    Update ProfitableGamble.js

--- ProfitableGamble.js
+++ ProfitableGamble.js
@@ -14,3 +14,7 @@ profitableGamble(0.9, 3, 2) ➞ true
 Notes
 A profitable gamble is a game that yields a positive net profit, where net profit is calculated in the following manner: net_outcome = probability_of_winning * prize - cost_of_playing.
 **/
+
+function profitableGamble(prob, prize, pay) {
+	
+}
