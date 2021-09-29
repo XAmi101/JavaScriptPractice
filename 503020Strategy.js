28add284409c5dd29eb99974904299bb97c5cc22
Ndawi <famousvacancy@gmail.com>
2021-09-28 23:59:53 -0400

    Update 503020Strategy.js

--- 503020Strategy.js
+++ 503020Strategy.js
@@ -8,7 +8,7 @@ fiftyThirtyTwenty(10000) ➞ { "Needs": 5000, "Wants": 3000, "Savings": 2000 }
 fiftyThirtyTwenty(50000) ➞ { "Needs": 25000, "Wants": 15000, "Savings": 10000 }
 fiftyThirtyTwenty(13450) ➞ { "Needs": 6725, "Wants": 4035, "Savings": 2690 }
 
-Notes
+Notes;
 N/A
 **/
 
