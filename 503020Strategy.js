6d423e6d24c75c59e6a221ff7f02a4cf3c251173
Ndawi <famousvacancy@gmail.com>
2021-09-29 23:58:15 -0400

    Update 503020Strategy.js

--- 503020Strategy.js
+++ 503020Strategy.js
@@ -8,10 +8,16 @@ fiftyThirtyTwenty(10000) ➞ { "Needs": 5000, "Wants": 3000, "Savings": 2000 }
 fiftyThirtyTwenty(50000) ➞ { "Needs": 25000, "Wants": 15000, "Savings": 10000 }
 fiftyThirtyTwenty(13450) ➞ { "Needs": 6725, "Wants": 4035, "Savings": 2690 }
 
-Notes;
+Notes
 N/A
 **/
 
 function fiftyThirtyTwenty(ati) {
-	
+     var needs= (ati/100)*50;
+     var wants= (ati/100)*30;
+     var savings= (ati/100)*20;
+
+    return ["Needs: " + needs + ", " + 
+    "Wants: " + wants + ", " +
+    "Savings: " + savings ];
 }
