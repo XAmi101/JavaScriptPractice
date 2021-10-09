6bb3b715e1b5bce32d09ad65b0e163f47fa4b647
Ndawi <famousvacancy@gmail.com>
2021-10-08 23:46:56 -0400

    Update LeapYrFunction.js

--- LeapYrFunction.js
+++ LeapYrFunction.js
@@ -18,5 +18,11 @@ You can solve the problem with a few lines of code.
 **/
 
 function leapYear(year) {
-	
+    if ( Number.isInteger(year/4) && 
+         Number.isInteger(year/100) &&  
+         Number.isInteger(year/400)) {
+      return true;
+    }else{
+      return false;
+    }	
 }
