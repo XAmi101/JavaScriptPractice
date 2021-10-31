df88787933befced49cf88f5a49f9ccdfb3c3a5b
Ndawi <famousvacancy@gmail.com>
2021-10-30 23:52:48 -0400

    Update ThePhScale.js

--- ThePhScale.js
+++ ThePhScale.js
@@ -20,3 +20,12 @@ function pHName(pH) {
 }
 
 console.log(pHName(9));
+
+
+/**
+const pHName = pH =>
+	pH < 0 || pH > 14 ? "invalid"
+    : pH < 7 ? "acidic"
+	: pH > 7 ? "alkaline"
+	: "neutral";
+**/
