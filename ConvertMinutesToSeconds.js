672cb67516efe022e251c80a402381c9a51f119b
Ndawi <famousvacancy@gmail.com>
2021-11-20 23:52:24 -0500

    Update ConvertMinutesToSeconds.js

--- ConvertMinutesToSeconds.js
+++ ConvertMinutesToSeconds.js
@@ -12,3 +12,8 @@ If you get stuck on a challenge, find help in the Resources tab.
 If you're really stuck, unlock solutions in the Solutions tab.
 
 **/
+
+
+function convert(minutes) {
+    return minutes * 60; 
+}
