f58f9be63f615f655d3cb9e66dded25bbcecba9f
Ndawi <famousvacancy@gmail.com>
2021-11-24 23:57:19 -0500

    Update ReturntheRemainderFromTwoNumbers.js

--- ReturntheRemainderFromTwoNumbers.js
+++ ReturntheRemainderFromTwoNumbers.js
@@ -13,3 +13,7 @@ Don't forget to return the result.
 If you get stuck on a challenge, find help in the Resources tab.
 If you're really stuck, unlock solutions in the Solutions tab.
 **/
+
+function remainder(x, y) {
+	
+}
