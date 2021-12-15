e404cd632b9c9f5c92978a685774d958901da5c0
Ndawi <famousvacancy@gmail.com>
2021-12-14 23:54:02 -0500

    Update HumanCatDogYears.js

--- HumanCatDogYears.js
+++ HumanCatDogYears.js
@@ -25,6 +25,12 @@ N/A
 
 **/
 function calculateYears(humanYears) {
-let catdogOne =15;
+  if(humanYears===1){
+    return [1,15,15];
+  }else if(humanYears==2){
+    return [2,24,24];
+  }else{
+    return [humanYears, 24+(humanYears-2)*4, 24+(humanYears-2)*5];
+  }
 	
 }
