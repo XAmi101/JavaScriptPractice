51ccc1f5f914237e6fcb6644ba272a09deac2d04
Ndawi <famousvacancy@gmail.com>
2022-01-07 22:54:38 -0500

    Update TwoMakesTen.js

--- TwoMakesTen.js
+++ TwoMakesTen.js
@@ -13,10 +13,13 @@ Don't forget to return the result.
 
 
 function makesTen(a, b) {
-   if(a||b==10||a+b === 10){
-      return true;
-   }else{
-      return false;
+    if(a ===10||b === 10){
+	return true
+    } else if(a+b === 10){
+	return true
+    } else {
+	return false
+    }
 }
 
-console.log(makesTen(1, 9));
+console.log(makesTen(10, 9));
