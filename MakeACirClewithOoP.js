13ecb90d658f87b5a3b6460c23c9d7b3082039cd
Ndawi <famousvacancy@gmail.com>
2022-01-18 00:01:03 -0500

    Update MakeACirClewithOoP.js

--- MakeACirClewithOoP.js
+++ MakeACirClewithOoP.js
@@ -6,12 +6,37 @@ For help with this class, I have provided you with a Rectangle constructor which
 Examples
 let circy = new Circle(11)
 circy.getArea()
-
 // Should return 380.132711084365
+
 let circy = new Circle(4.44)
 circy.getPerimeter()
-
 // Should return 27.897342763877365
+
 Notes
-**/
 Don't worry about floating point precision - I've factored in the chance that your answer may be more or less accurate than mine. This is more of a tutorial than a challenge so the topic covered may be considered advanced, yet the challenge is more simple - so if this challenge gets labelled as easy, don't worry too much.
+
+**/
+
+
+class Rectangle {
+  constructor(sideA, sideB) {
+    this.sideA = sideA
+    this.sideB = sideB
+  }
+  getArea(){return this.sideA*this.sideB}
+  getPerimeter(){return (this.sideA + this.sideB) *2}
+}
+
+
+class Circle {
+	// put your code here
+}
+
+
+// unquote and use run to test these cases:
+
+/*let q = new Circle(4.44);
+console.log(q.getArea());
+console.log(q.getPerimeter());
+*/
+
